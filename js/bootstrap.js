33e0ff2a14028723ef87c99ff2073c3f20e111aa
stelcheck <mtrudel@wizcorp.jp>
2014-06-10 14:48:58 +0900

    * Require the jQuery library specified in dependency

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -4,7 +4,13 @@
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  */
 
-if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript requires jQuery') }
+var jQuery;
+
+try {
+	jQuery = require('jquery');
+} catch (e) {
+	throw new Error('Bootstrap\'s JavaScript requires jQuery');
+}
 
 /* ========================================================================
  * Bootstrap: transition.js v3.1.1
